5d4158b283f6c0c4be2c3df0113a8c6ecfadb020
Richard Schneider <makaretu@gmail.com>
2016-10-26 12:27:19 +1300

    chore(package): initial package

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+The [change log](https://github.com/richardschneider/OpenBazaar-cli/releases) is automatically produced with
+the help of [semantic-release](https://github.com/semantic-release/semantic-release).
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-spawn-mocha');
+var coveralls = require('gulp-coveralls');
+
+var DEBUG = process.env.NODE_ENV === 'debug',
+    CI = process.env.CI === 'true';
+
+var paths = {
+    test: ['./test/**/*.js', '!test/{temp,temp/**}'],
+    source: ['./lib/*.js', './bin/*'],
+    doc: ['./doc/*.html', './doc/*.css']
+};
+paths.lint = paths.source.concat(paths.test);
+
+var plumberConf = {};
+
+if (CI) {
+    plumberConf.errorHandler = function(err) {
+        throw err;
+  };
+}
+
+gulp.task('lint', function() {
+  return gulp.src(paths.lint)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('istanbul', function () {
+  return gulp.src(paths.test, {read: false})
+    .pipe(mocha({
+      debugBrk: DEBUG,
+      R: 'spec',
+      istanbul: !DEBUG
+    }));
+});
+
+gulp.task('site', function() {
+//    require('./doc/make').run();    // dynamic documentation
+//    return gulp.src(paths.doc)      // static documentation
+//        .pipe(gulp.dest('./site/'));
+});
+
+gulp.task('coverage', function () {
+  return gulp.src('./coverage/lcov.info')
+    .pipe(coveralls());
+});
+
+gulp.task('test',    ['lint', 'istanbul']);
+gulp.task('ci',      ['test']);
+gulp.task('default', ['test']);
