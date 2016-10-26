0944b6bee58805d92931a26d27a4fe597e54c26f
Richard Schneider <makaretu@gmail.com>
2016-10-27 10:00:20 +1300

    refactor(options): define commanader options in the module

--- bin/ob-config.js
+++ bin/ob-config.js
@@ -5,13 +5,6 @@ const output = require('../lib/output');
 const config = require('../lib/server-config');
 
 app.commander
-    .option('--json', 'output result as JSON (default if object/array)')
-    .option('--text', 'output result as plain text (default if not object/array)')
-    .option('--csv', 'output result as CSV')
-    .option('--xml', 'output result as XML')
-    .option('-v, --verbose', 'verbose');
-
-app.commander
     .command('path')
     .description('display the path to the configuration file')
     .action(() => output.result(config.path))
