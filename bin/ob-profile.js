089be07462d50be654e95267094bbb4437ecbb1d
Richard Schneider <makaretu@gmail.com>
2016-10-27 20:21:48 +1300

    feat(ob): add profile command

---
+++ bin/ob-profile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const app = require('../lib/app');
+const output = require('../lib/output');
+const api = require('../lib/ob-api');
+
+app.commander
+    .command('list [target]')
+    .description('list the profile')
+    .alias('ls')
+    .action(target => api
+        .getJSON('profile', target)
+        .then(o => output.result(o)))
+    ;
+
+app.commander.parse(process.argv);
