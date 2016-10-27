2f7bd1c1f03617465bce1a2ff52b91a85da8fae1
Richard Schneider <makaretu@gmail.com>
2016-10-27 22:56:14 +1300

    feat(profile): set and get profile values

--- bin/ob-profile.js
+++ bin/ob-profile.js
@@ -10,7 +10,21 @@ app.commander
     .alias('ls')
     .action(target => api
         .getJSON('profile', { guid: target })
-        .then(o => output.result(o)))
+        .then(o => output.result(o.profile)))
+    ;
+
+app.commander
+    .command('get <key> [target]')
+    .description('get a profile value')
+    .action((key, target) => api
+        .getJSON('profile', { guid: target })
+        .then(o => output.result(o.profile[key])))
+    ;
+
+app.commander
+    .command('set <key> <value>')
+    .description('set a profile value')
+    .action((key, value) => api.postField('profile', key, value))
     ;
 
 app.commander.parse(process.argv);
