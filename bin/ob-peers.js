6e9f5ac706379eb671767154f96c9c2cc87585ae
Richard Schneider <makaretu@gmail.com>
2016-11-09 16:33:39 +1300

    feat(peers): ob peers --geo
    Add geographical info about the IP address.  Uses https://freegeoip.net for the data.

--- bin/ob-peers.js
+++ bin/ob-peers.js
@@ -1,15 +1,35 @@
 'use strict';
 
+const request = require('superagent');
 const app = require('../lib/app');
+const log = require('../lib/log');
 const output = require('../lib/output');
 const api = require('../lib/ob-api');
 
-app.commander.parse(process.argv);
+function ipinfo(ipaddr) {
+    return request
+        .get(`freegeoip.net/json/${ipaddr}`)
+        .then(res => res.body)
+    ;
+}
+
+app.commander
+    .option('--geo', 'add geographical info about the ip address')
+    .parse(process.argv)
+;
 
 api
     .getJSON('connected_peers')
     .then(o => {
         let peers = o.peers.map(p => p[0]);
-        output.result(peers);
+        if (app.options.geo) {
+            return Promise
+                .all(peers.map(ipinfo))
+                .then(result => output.result(result))
+                .catch(e => log.error(e))
+            ;
+        } else {
+            output.result(peers);
+        }
     })
 ;
